c097c4ae0abd1b2ac5ae3d78dae5a3333f64e066
Ken Irwin <kirwin@wittenberg.edu>
2019-01-09 10:50:15 -0500

    added getDistinct, applyConf to SearchTable

--- src/Wittlib/SearchTable.php
+++ src/Wittlib/SearchTable.php
@@ -6,27 +6,53 @@ use \atk4\dsql\Query;
 
 class SearchTable {
 
-  public function __construct () {
-    $this->c = \atk4\dsql\Connection::connect(DSN,USER,PASS);
+    public function __construct () {
+        $this->c = \atk4\dsql\Connection::connect(DSN,USER,PASS);
+        
+        $this->q = $this->c->dsql(); //new Query();
+        /*->table('user')->where('id',1)->field('name');
+          print($q->render());
+          print_r($q->params);
+        */
+    }
+    public function getFirst($table, $field) {
+        $this->q->table($table)->field($field)->limit(1,0)->order($field.' ASC');
+        $return = $this->q->getOne();
+        return $return;
+    }
+    public function getLast($table, $field) {
+        $this->q->table($table)->field($field)->limit(1,0)->order($field.' DESC');
+        $return = $this->q->getOne();
+        return $return;
+    }
     
-    $this->q = $this->c->dsql(); //new Query();
-      /*->table('user')->where('id',1)->field('name');
-    print($q->render());
-    print_r($q->params);
-      */
-  }
-  public function getFirst($table, $field) {
-    $this->q->table($table)->field($field)->limit(1,0)->order($field.' ASC');
-    $return = $this->q->getOne();
-    return $return;
-  }
-  public function getLast($table, $field) {
-    $this->q->table($table)->field($field)->limit(1,0)->order($field.' DESC');
-    $return = $this->q->getOne();
-    return $return;
-  }
-    public function booleanAnd(string $table, string $terms, array $fields, array $conf = []) {
-    $this->q->table($table);
+    public function getDistinct(string $table, string $field, array $conf = [])
+    {
+        if (! array_key_exists('orderby', $conf)) {
+            $conf['orderby'] = $field;
+        }
+        $this->q->table($table)->field($field)->option('distinct');
+        $this->applyConf($conf);
+        $return = array();
+        foreach ($this->q->get() as $row) {
+            array_push($return, $row[$field]);
+        }
+        return $return;
+    }
+
+    private function applyConf($conf) {
+        if (array_key_exists('direction', $conf)) {
+            $direction = $conf['direction'];
+        }
+        else { $direction = 'ASC'; }
+        if (array_key_exists('orderby', $conf)) {
+            $this->q->order($conf['orderby'] .' '.$direction);
+        }
+
+    }
+
+    public function booleanAnd(string $table, string $terms, array $fields, array $conf = [], bool $return_results = true) {
+        $this->q->table($table);
     $terms = preg_split('/\s+/',$terms);
 
     foreach ($terms as $term) {
@@ -38,10 +64,11 @@ class SearchTable {
       $or_str = [$or_arr];
       $this->q->where($or_arr);
     }
+    /* KEN: can you replace this with ->applyConf() ? */
     if (array_key_exists('orderby', $conf)) {
         $this->q->order($conf['orderby']);
     }
-    return $this->q->get();
+    if ($return_results) { return $this->q->get(); }
     }
 
 }
