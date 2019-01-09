9292a49b78c60c5a752441d1828ee65189e9f9ca
Ken Irwin <kirwin@wittenberg.edu>
2019-01-08 19:41:54 -0500

    moved test to views/

--- views/test.php
+++ views/test.php
@@ -2,15 +2,16 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require_once __DIR__ . '/vendor/autoload.php';
-require_once 'config.php';
+require_once '../vendor/autoload.php';
+require_once '../config.php';
+getConfig('witt_pubs');
 
 use Wittlib\SearchTable;
 
 header('Content-type: text/plain');
 $db = new SearchTable();
 //print ($db->getLast('pholeos','year'));
-$db->booleanAnd('pholeos',['photo'],['genre','title']);
+$db->booleanAnd('pholeos','photo',['genre','title']);
 var_dump($db->q->render());
 var_dump($db->q->get());
 try {
