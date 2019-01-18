ead3330aa7b1a3a888793a19f5f3a34fcb09c6e3
Ken Irwin <kirwin@wittenberg.edu>
2019-01-18 15:18:24 -0500

    Redirect returns errors as appropriate

--- src/Wittlib/Redirect.php
+++ src/Wittlib/Redirect.php
@@ -9,6 +9,9 @@ class Redirect {
         $this->c = \atk4\dsql\Connection::connect(DSN,USER,PASS);
         $this->errors = array();
         $this->resolveURL($id);
+        if ($this->hasErrors()) {
+            return false;
+        }
         $this->checkCurrent();
         $this->getReplacements($this->id);
     }
@@ -16,23 +19,29 @@ class Redirect {
     private function resolveURL ($id) {
         $this->q = $this->c->dsql(); //new Query();
         $this->q->table('db_new')->where('ID',$id);
-        $info = $this->q->get()[0];
-        $this->id = $info['ID'];
-        foreach($info as $k=>$v) {
-            $this->$k = $v;
+        $info_arr = $this->q->get();
+        if (array_key_exists(0, $info_arr)) {
+            $info = $this->q->get()[0];
+            $this->id = $info['ID'];
+            foreach($info as $k=>$v) {
+                $this->$k = $v;
+            }
+            if (preg_match('/\d+/',$this->route_to_db)) {
+                $temp=$this->route_to_db;
+                $this->route_to_db = null;
+                $this->resolveURL($temp);
+            }
         }
-        if (preg_match('/\d+/',$this->route_to_db)) {
-            $temp=$this->route_to_db;
-            $this->route_to_db = null;
-            $this->resolveURL($temp);
+        else {
+            $this->errors['not_found'] = 'This database was not found.';
         }
     }
     private function checkCurrent() {
         if (preg_match('/\d\d\d\d-\d\d-\d\d/',$this->cancelled)) {
-            array_push($this->errors, 'This database was cancelled '.date('M Y', strtotime($this->cancelled)).PHP_EOL);
+            $this->errors['cancelled'] = 'This database ('.$this->title.') was cancelled '.date('M Y', strtotime($this->cancelled)).'.'.PHP_EOL;
         }
         if ($this->suppress == 'Y') { 
-            array_push($this->errors, 'This database is no longer available: '.$this->title.PHP_EOL);
+            $this->errors['suppressed'] = 'This database ('.$this->title.') is no longer available.'.PHP_EOL;
         }
     }
     public function hasErrors() {
@@ -45,19 +54,24 @@ class Redirect {
     }
 
     public function getReplacements($id) {
-        $replacements = preg_split('/;/',$this->use_new_db);
-        $titles = array();
-        foreach ($replacements as $id) {
-            $titles[$id] = $this->getTitle($id);
-        }
-        if (sizeof($titles) > 0) {
-            $urls = 'Try these databases as alternatives: ';
-            foreach ($titles as $db_id => $title) {
-                $urls .= '<li><a href="/redirect.php?'.$id.'">'.$title.'</a>'.PHP_EOL;
+        if ($this->use_new_db !== null) { 
+            $replacements = preg_split('/;/',$this->use_new_db);
+            $titles = array();
+            var_dump($titles);
+            var_dump($replacements);
+            foreach ($replacements as $id) {
+                $titles[$id] = $this->getTitle($id);
             }
+            if (sizeof($titles) > 0) {
+                $urls = 'Try these databases as alternatives: ';
+                foreach ($titles as $db_id => $title) {
+                    $urls .= '<li><a href="/redirect.php?'.$id.'">'.$title.'</a>'.PHP_EOL;
+                }
+            }
+            $this->alternatives = $urls;
         }
-        $this->alternatives = $urls;
     }
+
     public function getTitle($id) {
         $next = $this->c->dsql(); //new Query();
         $next->table('db_new')->where('ID',$id)->field('title');
