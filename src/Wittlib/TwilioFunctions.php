314911d9b66ae537b7fbcc09f91e748167a7f909
Ken Irwin <kirwin@wittenberg.edu>
2019-03-28 16:55:05 -0400

    TwilioFunctions support send

--- src/Wittlib/TwilioFunctions.php
+++ src/Wittlib/TwilioFunctions.php
@@ -2,6 +2,7 @@
 
 namespace Wittlib;
 use Twilio\Rest\Client;
+use Twilio\Exceptions\RestException;
 /*
   in application, include config from app root 
   and call getConfig()
@@ -14,6 +15,25 @@ class TwilioFunctions {
         $this->token = $token;
         $this->from = $from;
     }
+    
+    public function send ($to, $message) {
+            $client = new Client($this->sid, $this->token);
+    
+            try {
+                $client->messages->create(
+                    $to,
+                    array (
+                        'from' => $this->from,
+                        'body' => $message
+                    )
+                );
+                return true;
+            } catch (RestException $e) {
+                mail ('kirwin@wittenberg.edu','Twilio Failed',$e);
+                return false;
+            }
+    }
+    
     public function carrierLookup ($number) {
         $client = new Client($this->sid, $this->token);
         
--- tests/TwilioFunctionsTest.php
+++ tests/TwilioFunctionsTest.php
@@ -13,14 +13,32 @@ class TwilioFunctionsTest extends TestCase {
     public function setUp() {
         $this->fn = new TwilioFunctions(TWILIO_SID,TWILIO_TOKEN,TWILIO_FROM);
     }
-    
-    public function testCarrierLookupFindsCarrier() {
-        print 'Test commented out so as not to incur Twilio fees. Uncomment if you need to test Twilio functionality'.PHP_EOL;
+
         /*
+    public function testSendSms() {
+        $success = $this->fn->send(TWILIO_REAL_RECIPIENT, 'Test message from function: '. __FUNCTION__ . PHP_EOL . 'File: '.__FILE__ . PHP_EOL . 'Line: '. __LINE__);
+        $this->assertTrue($success);
+    }
+
+
+    public function testCarrierLookupFindsCarrier() {
         $ken = '+19377279713';
         $carrier = $this->fn->carrierLookup($ken);
         $this->assertRegExp('/verizon/i', $carrier);
+    }
         */
+
+    public function testMessage() {
+        print '***************'.PHP_EOL;
+        print 'Tests commented out so as not to incur Twilio fees.'.PHP_EOL.'Uncomment if you need to test Twilio functionality'.PHP_EOL;
+        print '***************'.PHP_EOL;
+        $this->assertTrue(true);
+    }
+    
+
+    public function tearDown () {
+        unset ($this->fn);
     }
 }
+
 
