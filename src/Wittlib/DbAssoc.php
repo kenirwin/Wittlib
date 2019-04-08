83ee95d2aacfa14ccc2e6685bacf83aea1a59f79
Ken Irwin <kirwin@wittenberg.edu>
2019-04-08 18:18:57 +0000

    basic setup for DbAssoc and tests

---
+++ src/Wittlib/DbAssoc.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Wittlib;
+
+use \atk4\dsql\Query;
+
+class DbAssoc {
+    public function __construct () {
+        $this->c = \atk4\dsql\Connection::connect(DSN,USER,PASS);
+    }
+}
+
